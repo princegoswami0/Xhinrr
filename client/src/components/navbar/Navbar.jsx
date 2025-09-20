8a530c2509fabdf79c251e678c36479770527644
Prince <prince00goswami@gmail.com>
2025-09-20 12:29:24 +0530

    reintialise the project due to error and provide docs related to css layout Conclusion : -  project starts with no error and proper docs and flows and currently working on Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import "./Navbar.scss"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          <span className='text'>Xhinrr</span>
+          <span className='dot'>.</span>
+        </div>
+        <div className='nav-links'>
+          <span>Xhinrr Pro</span>
+          <span>Explore</span>
+          <span>EN</span>
+          <span>Sign In</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
